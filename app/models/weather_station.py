fb8d8c31adc726367cdb5e9899c303421332c35f
Oliver Neff <olineff@aol.com>
2026-07-26 18:07:47 +0200

    Stations-Feld als Array zu WeatherData hinzufügen
    - WeatherStation-Array in WeatherData-Modell aufnehmen
    - Adapter liefert aktuelle Station als Station-Array zurück

--- app/models/weather_station.py
+++ app/models/weather_station.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 
 class WeatherStation(BaseModel):
+    source: str | None = None
     name: str | None = None
     lat: float | None = None
     lon: float | None = None
